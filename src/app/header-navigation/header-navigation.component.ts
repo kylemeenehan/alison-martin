285affa4f4529990c563c7cded5c9a26f127e74e
chris <chris@topher.co.za>
2018-09-01 11:13:00 +0200

    fixed navigation header displaying.

--- src/app/header-navigation/header-navigation.component.ts
+++ src/app/header-navigation/header-navigation.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '../models/title';
-import { Location } from '@angular/common';
-import { debug } from 'util';
 
 @Component({
   selector: 'app-header-navigation',
@@ -11,18 +9,14 @@ import { debug } from 'util';
 })
 export class HeaderNavigationComponent implements OnInit {
   titles;
-  showNav: number = 0;
-  location: Location;
-  constructor(location: Location) { this.location = location; }
-    
-  
+  constructor() { }
+
+
 
   ngOnInit() {
     this.titles = Title.Titles;
-    if (location.pathname !== '/home' && location.pathname !=='/'){
-      this.showNav = 1;
-    }  
-     
+
+
   }
 
 }
--- src/app/models/title.ts
+++ src/app/models/title.ts
@@ -1,9 +1,9 @@
 export class Title{
     static Titles = [
-        { "url": "directing", "name": "Director" },
-        { "url": "art-directing", "name": "Art Director" },
-        { "url": "collection/1", "name": "Art" },
-        { "url": "photography", "name": "Photography" }];
+        { 'url': 'directing', 'name': 'Director' },
+        { 'url': 'art-directing', 'name': 'Art Director' },
+        { 'url': 'collection/1', 'name': 'Art' },
+        { 'url': 'photography', 'name': 'Photography' }];
     url: string;
     name: string;
 }
