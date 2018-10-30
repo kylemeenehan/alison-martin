45fb848eb55124effbfe26910da0470d6eb43e75
chris <chris@topher.co.za>
2018-10-30 23:45:27 +0200

    Collections: added sorting based on sort order field in api

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -26,13 +26,23 @@ export class CollectionComponent implements OnInit {
     if(!this.api.collections || !this.api.works) {
       await this.api.downloadAll();
     }
-    this.collections = this.api.collections;
+    this.collections = this.sortCollections(this.api.collections);
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.container.nativeElement.scrollTop = 0;
-      const id = params.get('id');
+      let id = params.get('id');
+      if (id == '0') {
+        id = '4'; // this was the id of the collection that we want to display first if navtigating to the page the first time. Probably not anymore though.
+      }
       this.collection = this.collections.find(collection => collection.id == id);
       this.works = this.api.getWorksByCollection(id);
     });
   }
 
+  sortCollections = (collections: any) =>
+  collections.sort((a, b) => {
+    if (Number(a.sort_order) < Number(b.sort_order)) { return -1; }
+    if (Number(a.sort_order) === Number(b.sort_order)) { return 0; }
+    if (Number(a.sort_order) > Number(b.sort_order)) { return 1; }
+    return 0;
+  })
 }
